d37c2476c75d5e6f2f4cd47f1226cb6ac3e56808
teemvat <teemu.vataja@metropolia.fi>
2024-09-19 15:11:08 +0300

    arrivaprocess

---
+++ java/simu/framework/ArrivalProcess.java
@@ -0,0 +1,21 @@
+package simu.framework;
+import eduni.distributions.*;
+import simu.model.EventType;
+public class ArrivalProcess {
+	
+	private ContinuousGenerator generator;
+	private EventList eventList;
+	private EventType type;
+
+	public ArrivalProcess(ContinuousGenerator g, EventList el, EventType type){
+		this.generator = g;
+		this.eventList = el;
+		this.type = type;
+	}
+
+	public void generateNext(){
+		Event evt = new Event(type, Clock.getInstance().getTime()+ generator.sample());
+		eventList.add(evt);
+	}
+
+}
