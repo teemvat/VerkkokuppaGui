5b4bd864e306eed743c85efdbaf31608cd226854
mirap <mira.peryshko@metropolia.fi>
2024-10-09 01:30:15 +0300

    Tehty uusi dao-kansio ja sinne omat daot orderille ja simulationille, molemmissa samat toiminnot

---
+++ src/main/java/dao/OrderDAO.java
@@ -0,0 +1,42 @@
+package dao;
+
+import simu.model.entity.Order;
+import java.util.List;
+import jakarta.persistence.EntityManager;
+
+
+public class OrderDAO {
+
+    public void persist(Order o) {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        em.getTransaction().begin();
+        em.persist(o);
+        em.getTransaction().commit();
+    }
+
+    public Order find(int id) {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        Order o = em.find(Order.class, id);
+        return o;
+    }
+
+    public List<Order> findAll() {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        List<Order> orders = em.createQuery("select e from Order e").getResultList();
+        return orders;
+    }
+
+    public void update(Order o) {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        em.getTransaction().begin();
+        em.merge(o);
+        em.getTransaction().commit();
+    }
+
+    public void delete(Order o) {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        em.getTransaction().begin();
+        em.remove(o);
+        em.getTransaction().commit();
+    }
+}
---
+++ src/main/java/dao/SimulationDAO.java
@@ -0,0 +1,42 @@
+package dao;
+
+import jakarta.persistence.EntityManager;
+import simu.model.entity.Simulation;
+import java.util.List;
+
+
+public class SimulationDAO {
+
+    public void persist(Simulation s) {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        em.getTransaction().begin();
+        em.persist(s);
+        em.getTransaction().commit();
+    }
+
+    public Simulation find(int id) {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        Simulation s = em.find(Simulation.class, id);
+        return s;
+    }
+
+    public List<Simulation> findAll() {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        List<Simulation> simulations = em.createQuery("select e from Order e").getResultList();
+        return simulations;
+    }
+
+    public void update(Simulation s) {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        em.getTransaction().begin();
+        em.merge(s);
+        em.getTransaction().commit();
+    }
+
+    public void delete(Simulation s) {
+        EntityManager em = datasource.MariaDbJpaConnection.getInstance();
+        em.getTransaction().begin();
+        em.remove(s);
+        em.getTransaction().commit();
+    }
+}
