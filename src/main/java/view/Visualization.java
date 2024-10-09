9f1c9cd5e9fc2ce9b1cdc770981b60961ff94624
Itz-Ezu <eetu.oinonen@metropolia.fi>
2024-10-09 21:46:30 +0300

    Melkein valmis part2

--- src/main/java/view/Visualization.java
+++ src/main/java/view/Visualization.java
@@ -2,44 +2,43 @@ package view;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 public class Visualization extends Canvas implements IVisualization {
-	private Image icon = new Image("file:src/main/resources/package.png");
+
 	private final GraphicsContext gc;
-	
+
 	double i = 0;
 	double j = 10;
-	
-	
+
+
 	public Visualization(int w, int h) {
 		super(w, h);
 		gc = this.getGraphicsContext2D();
 		clearScreen();
 	}
-	
+
 
 	public void clearScreen() {
-		gc.setFill(Color.web("#c7c7c7"));
+		gc.setFill(Color.YELLOW);
 		gc.fillRect(0, 0, this.getWidth(), this.getHeight());
 	}
-	
-	public void newPackage() {
-		gc.setFill(Color.web("#c7c7c7"));
-		gc.fillOval(i,j,10,10);
-		
-		i = (i + 10) % this.getWidth();
-		j = (j + 12) % this.getHeight();
-		if (i==0) j+=10;
 
-		gc.drawImage(icon, i, j);
+	public void newPackage() {
+		gc.setFill(Color.RED);
+		gc.fillOval(i,j,10,10);
+
+		i = (i + 10) % this.getWidth();
+		//j = (j + 12) % this.getHeight();
+		if (i==0) j+=10;
 	}
 
 	public void movePackage(){
 		i = (i - 10) % this.getWidth();
 		if (i<0) j-=10;
 
+		gc.setFill(Color.YELLOW);
+		gc.fillRect(i,j,10,10);
 	}
-	
+
 }
