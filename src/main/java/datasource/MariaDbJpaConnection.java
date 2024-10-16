588935d111c728b112fa036383cc97893659520f
mirap <mira.peryshko@metropolia.fi>
2024-10-17 02:59:01 +0300

    New JavaDoc

--- src/main/java/datasource/MariaDbJpaConnection.java
+++ src/main/java/datasource/MariaDbJpaConnection.java
@@ -2,14 +2,33 @@ package datasource;
 
 import jakarta.persistence.*;
 
+
+/**
+ * This class is used to create a connection to the MariaDB database using JPA.
+ * It is a singleton class.
+ */
 public class MariaDbJpaConnection {
 
+    /**
+     * The EntityManagerFactory object is used to create an EntityManager object.
+     */
     private static EntityManagerFactory emf = null;
+
+    /**
+     * The EntityManager object is used to perform operations on the database.
+     */
     private static EntityManager em = null;
 
-    public static EntityManager getInstance() {
-        // you need to add synchronization if you run in a multi-threaded environment
 
+    /**
+     * This method is used to create an EntityManager object:
+     * 1. If the EntityManager object is not created, then it creates an EntityManager object.
+     * 2. If the EntityManagerFactory object is not created, then it creates an EntityManagerFactory object.
+     * 3. It creates an EntityManager object using the EntityManagerFactory object.
+     *
+     * @return EntityManager object
+     */
+    public static EntityManager getInstance() {
         if (em==null) {
             if (emf==null) {
                 emf = Persistence.createEntityManagerFactory("SimulatorMariaDbUnit");
