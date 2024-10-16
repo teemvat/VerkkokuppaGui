3a4749b7ee4ed7ae99dce61d1815405584412008
teemvat <teemu.vataja@metropolia.fi>
2024-10-16 22:21:07 +0300

    Testejä

--- src/test/java/ControllerTest.java
+++ src/test/java/ControllerTest.java
@@ -6,12 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import simu.model.entity.Simulation;
 import view.ISimulatorUI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ControllerTest {
